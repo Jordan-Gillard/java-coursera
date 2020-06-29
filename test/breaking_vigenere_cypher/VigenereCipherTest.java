afc5966cc054a6cb6b0af940f0ab049177bf5588
Jordan <jordan-gillard@outlook.com>
2020-06-29 10:10:06 -0500

    test: Adds test for VigenereCipher
    This commit creates a new VigenereCipherTest class. This class
    contains one test for encrypting a message using key "ROME"
    and decrypting it. The test asserts that the beginning of the
    encrypted message matches a specific encrypted string, and that
    the decrypted message is the same as the original message.

---
+++ test/breaking_vigenere_cypher/VigenereCipherTest.java
@@ -0,0 +1,21 @@
+package breaking_vigenere_cypher;
+
+import edu.duke.FileResource;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VigenereCipherTest {
+    @Test
+    public void testEncryptAndDecryptMessageWithKeyRome() {
+        int[] key = {17, 14, 12, 4};
+        VigenereCipher vc = new VigenereCipher(key);
+        FileResource fr = new FileResource("breaking_vigenere_cypher/text_files/titus-small.txt");
+        String message = fr.asString();
+        String encryptedMessage = vc.encrypt(message);
+        String expectedBeginning = "Tcmp-pxety mj nikhqv htee mrfhtii tyv";
+        assertTrue(encryptedMessage.startsWith(expectedBeginning));
+        String decryptedMessage = vc.decrypt(encryptedMessage);
+        assertEquals(message, decryptedMessage);
+    }
+}
